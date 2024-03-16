8f61e8217fe2f4805222479c4596dc420fb1a4fc
Wenyu Zheng <zhengwy229@gmail.com>
2024-03-16 00:22:31 +0000

    Created ingredients list api

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -9,7 +9,7 @@ from rest_framework import (
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
 
 
@@ -48,4 +48,17 @@ class TagViewSet(mixins.DestroyModelMixin,
 
     def get_queryset(self):
         """Retrieve tags for authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredientViewSet(mixins.ListModelMixin,
+                        viewsets.GenericViewSet):
+    """Manage ingredients in the datatbase."""
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve ingredients for authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
